5272bec0ef682a59e4be1821ae264783dd2bc704
totalfruitid-cpu <totalfruit.id@gmail.com>
2026-04-20 11:37:42 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,34 +1,15 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(request) {
-  const token = request.cookies.get('authToken')?.value;
-  const role = request.cookies.get('userRole')?.value;
-  const { pathname } = request.nextUrl;
-
-  const isLoginPage = pathname === '/';
-  const isAdminPage = pathname.startsWith('/admin');
-  const isKasirPage = pathname.startsWith('/kasir');
-
-  if (!token && (isAdminPage || isKasirPage)) {
-    return NextResponse.redirect(new URL('/', request.url));
+export function middleware(req) {
+  const token = req.cookies.get('authToken')?.value;
+  
+  if (!token) {
+    return NextResponse.redirect(new URL('/', req.url));
   }
-
-  if (token && isLoginPage) {
-    if (role === 'admin') return NextResponse.redirect(new URL('/admin', request.url));
-    if (role === 'kasir') return NextResponse.redirect(new URL('/kasir', request.url));
-  }
-
-  if (token && role === 'kasir' && isAdminPage) {
-    return NextResponse.redirect(new URL('/kasir', request.url));
-  }
-
-  if (token && role === 'admin' && isKasirPage) {
-    return NextResponse.redirect(new URL('/admin', request.url));
-  }
-
+  
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/', '/admin/:path*', '/kasir/:path*'],
+  matcher: ['/admin/:path*', '/kasir/:path*'],
 };
