a283206604a204dfc02779e7732ed3b03e21e645
totalfruitid-cpu <totalfruit.id@gmail.com>
2026-04-20 10:56:21 +0700

    Delete route.js

--- app/api/logout/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
-
-export async function POST() {
-  cookies().delete('authToken');
-  cookies().delete('userRole');
-  return NextResponse.json({ success: true });
-}
