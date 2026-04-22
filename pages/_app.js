bea3abc540e19eb3d9361a58228a7992fbf26ebf
totalfruitid-cpu <totalfruit.id@gmail.com>
2026-04-22 11:42:00 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,42 @@
 import '../styles/globals.css'
+import { useEffect, useState } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "../lib/firebase"
+import { useRouter } from "next/router"
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (u) => {
+      setUser(u)
+      setLoading(false)
+    })
+
+    return () => unsub()
+  }, [])
+
+  useEffect(() => {
+    if (loading) return
+
+    const publicRoutes = ["/login"]
+
+    const isPublic = publicRoutes.includes(router.pathname)
+
+    if (!user && !isPublic) {
+      router.replace("/login")
+    }
+  }, [user, loading, router.pathname])
+
+  if (loading) {
+    return (
+      <div style={{ padding: 20 }}>
+        Loading system...
+      </div>
+    )
+  }
+
+  return <Component {...pageProps} user={user} />
 }
